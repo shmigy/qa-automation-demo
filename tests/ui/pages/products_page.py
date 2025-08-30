dd124fcbdb36c3e6f0a2f69d9689a85bffff4416
shmigy <damir.smigovec@gmail.com>
2025-08-31 00:07:07 +0200

    prducts page update

--- tests/ui/pages/products_page.py
+++ tests/ui/pages/products_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class ProductsPage:
     def __init__(self, driver):
