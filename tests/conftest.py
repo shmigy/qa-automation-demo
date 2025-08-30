83152eacc237b893b97aa21c84b5adc5101eb116
shmigy <damir.smigovec@gmail.com>
2025-08-30 23:59:03 +0200

    conftest - headless chrome

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
     chrome_options = Options()
+    chrome_options.add_argument("--headless=new") 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
